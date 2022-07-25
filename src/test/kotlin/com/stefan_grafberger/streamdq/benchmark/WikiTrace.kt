2323b6bab054ae17990b9e42f16e381b7a5ffaa6
Stefan Grafberger <18442112+stefan-grafberger@users.noreply.github.com>
2022-07-25 15:28:48 +0200

    Struggling to get example to work

---
+++ src/test/kotlin/com/stefan_grafberger/streamdq/benchmark/WikiTrace.kt
@@ -0,0 +1,67 @@
+package com.stefan_grafberger.streamdq.benchmark
+
+import de.bytefish.jtinycsvparser.CsvParser
+import de.bytefish.jtinycsvparser.CsvParserOptions
+import de.bytefish.jtinycsvparser.builder.IObjectCreator
+import de.bytefish.jtinycsvparser.mapping.CsvMapping
+import de.bytefish.jtinycsvparser.tokenizer.StringSplitTokenizer
+import org.apache.flink.streaming.api.functions.source.SourceFunction
+import org.apache.flink.streaming.api.functions.source.SourceFunction.SourceContext
+import java.lang.Exception
+import java.nio.charset.StandardCharsets
+import java.nio.file.FileSystems
+import java.nio.file.Path
+
+data class WikiTrace @JvmOverloads constructor(
+    var counter: Int? = null,
+    var timestamp: String? = null,
+    var url: String? = null,
+    var dbUpdate: String? = null
+)
+
+class WikiTraceMapper(creator: IObjectCreator<WikiTrace?>) : CsvMapping<WikiTrace?>(creator) {
+    init {
+        mapProperty(0, Int::class.java) { wikiTrace: WikiTrace?, intValue: Int -> wikiTrace?.counter = intValue }
+        mapProperty(1, String::class.java) { wikiTrace: WikiTrace?, stringValue: String -> wikiTrace?.timestamp = stringValue }
+        mapProperty(2, String::class.java) { wikiTrace: WikiTrace?, stringValue: String -> wikiTrace?.url = stringValue }
+        mapProperty(3, String::class.java) { wikiTrace: WikiTrace?, stringValue: String -> wikiTrace?.dbUpdate = stringValue }
+    }
+}
+
+class WikiTraceSource(private var sourceFilePath: String) : SourceFunction<WikiTrace> {
+    @Volatile
+    private var isRunning = true
+    private var counter = 0
+
+    @Throws(Exception::class)
+    override fun run(sourceContext: SourceContext<WikiTrace?>) {
+        val csvSourcePath: Path = FileSystems.getDefault().getPath(sourceFilePath)
+        createWikiTraceParser()
+            .readFromFile(csvSourcePath, StandardCharsets.ISO_8859_1)
+            .filter { x -> x.isValid }
+            .map { x -> x.result }
+            .use { stream ->
+                val iterator: Iterator<WikiTrace?> = stream.iterator()
+                while (isRunning && iterator.hasNext()) {
+                    sourceContext.collect(iterator.next())
+                    counter++
+                }
+            }
+    }
+
+    override fun cancel() {
+        isRunning = false
+    }
+
+    companion object {
+        fun createWikiTraceParser(): CsvParser<WikiTrace?> {
+            return CsvParser(
+                CsvParserOptions(
+                    false,
+                    StringSplitTokenizer(" ", false)
+                ),
+                WikiTraceMapper { WikiTrace() }
+            )
+        }
+    }
+}
---
+++ src/test/kotlin/com/stefan_grafberger/streamdq/benchmark/WikiTraceBenchmark.kt
@@ -0,0 +1,24 @@
+package com.stefan_grafberger.streamdq.benchmark
+
+import org.apache.flink.streaming.api.datastream.DataStream
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class WikiTraceBenchmark {
+
+    private val resourceName: String = "/clickstream-zhwiki-2022-06.tsv"
+
+    private fun getResourceFileName(): String {
+        return javaClass.getResource(resourceName).toString()
+    }
+
+    @Test
+    fun `test load WikiTrace`() {
+        val env = StreamExecutionEnvironment.createLocalEnvironment(1)
+        val inputStream: DataStream<WikiTrace> = env.addSource(WikiTraceSource(WikiTraceBenchmark().getResourceFileName()))
+        val result = inputStream.executeAndCollect().iterator().asSequence().toList()
+        println(result)
+    }
+}
