4c0850aa296852a6be12c1c45d44c218c4d38fed
Tong <tong.wu4@student.uva.nl>
2023-03-26 18:13:25 +0200

    Implement simple threshold strategy and adjust interface

--- src/main/kotlin/com/stefan_grafberger/streamdq/anomalydetection/AnomalyDetectionStrategy.kt
+++ src/main/kotlin/com/stefan_grafberger/streamdq/anomalydetection/AnomalyDetectionStrategy.kt
@@ -1,9 +1,11 @@
 package com.stefan_grafberger.streamdq.anomalydetection
 
+import com.stefan_grafberger.streamdq.anomalydetection.model.Anomaly
+
 /**
  * The common interface for all anomaly detection strategies
  */
 interface AnomalyDetectionStrategy {
     fun detect(dataStream: List<Double>,
-               searchInterval: Pair<Int, Int>)
+               searchInterval: Pair<Int, Int>): MutableCollection<Pair<Int, Anomaly>>
 }
--- src/main/kotlin/com/stefan_grafberger/streamdq/anomalydetection/thresholdstrategy/SimpleThresholdStrategy.kt
+++ src/main/kotlin/com/stefan_grafberger/streamdq/anomalydetection/thresholdstrategy/SimpleThresholdStrategy.kt
@@ -1,9 +1,27 @@
 package com.stefan_grafberger.streamdq.anomalydetection.thresholdstrategy
 
 import com.stefan_grafberger.streamdq.anomalydetection.AnomalyDetectionStrategy
+import com.stefan_grafberger.streamdq.anomalydetection.model.Anomaly
 
 class SimpleThresholdStrategy : AnomalyDetectionStrategy {
-    override fun detect(dataStream: List<Double>, searchInterval: Pair<Int, Int>) {
-        TODO("Not yet implemented")
+
+    /**
+     * Search for anomalies in a stream of data
+     *
+     * @param dataStream     The data contained in a List of Doubles
+     * @param searchInterval The value range between which anomalies to be detected [a,b].
+     * @return A list of Pairs with the indexes of anomalies in the interval and their corresponding wrapper object.
+     */
+    override fun detect(dataStream: List<Double>, searchInterval: Pair<Int, Int>): MutableCollection<Pair<Int, Anomaly>> {
+        val (lowerBound, upperBound) = searchInterval
+        require(lowerBound <= upperBound) { "The start of interval must be lower than the end" }
+        val res : MutableCollection<Pair<Int, Anomaly>> = mutableListOf()
+        dataStream.forEachIndexed { index, value ->
+            if (value < lowerBound || value > upperBound) {
+                val detail = "[SimpleThresholdStrategy]: data value $value is not in [$lowerBound, $upperBound]}"
+                res.add(Pair(index, Anomaly(value, 1.0, detail)))
+            }
+        }
+        return res
     }
 }
