47a1387b0ae7f2711107af509f5eb35b52c04cae
Tong <tong.wu4@student.uva.nl>
2023-03-27 20:27:08 +0200

    add prerequisite for SimpleThresholdStrategy

--- src/main/kotlin/com/stefan_grafberger/streamdq/anomalydetection/thresholdstrategy/SimpleThresholdStrategy.kt
+++ src/main/kotlin/com/stefan_grafberger/streamdq/anomalydetection/thresholdstrategy/SimpleThresholdStrategy.kt
@@ -3,7 +3,13 @@ package com.stefan_grafberger.streamdq.anomalydetection.thresholdstrategy
 import com.stefan_grafberger.streamdq.anomalydetection.AnomalyDetectionStrategy
 import com.stefan_grafberger.streamdq.anomalydetection.model.Anomaly
 
-class SimpleThresholdStrategy : AnomalyDetectionStrategy {
+class SimpleThresholdStrategy(
+        lowerBound: Double,
+        upperBound: Double) : AnomalyDetectionStrategy {
+
+    init {
+        require(lowerBound <= upperBound) { "The lower bound must be smaller or equal to the upper bound." }
+    }
 
     /**
      * Search for anomalies in a stream of data
@@ -13,12 +19,12 @@ class SimpleThresholdStrategy : AnomalyDetectionStrategy {
      * @return A list of Pairs with the indexes of anomalies in the interval and their corresponding wrapper object.
      */
     override fun detect(dataStream: List<Double>, searchInterval: Pair<Int, Int>): MutableCollection<Pair<Int, Anomaly>> {
-        val (lowerBound, upperBound) = searchInterval
-        require(lowerBound <= upperBound) { "The start of interval must be lower than the end" }
-        val res : MutableCollection<Pair<Int, Anomaly>> = mutableListOf()
+        val (startInterval, endInterval) = searchInterval
+        require(startInterval <= endInterval) { "The start of interval must be lower than the end" }
+        val res: MutableCollection<Pair<Int, Anomaly>> = mutableListOf()
         dataStream.forEachIndexed { index, value ->
-            if (value < lowerBound || value > upperBound) {
-                val detail = "[SimpleThresholdStrategy]: data value $value is not in [$lowerBound, $upperBound]}"
+            if (value < startInterval || value > endInterval) {
+                val detail = "[SimpleThresholdStrategy]: data value $value is not in [$startInterval, $endInterval]}"
                 res.add(Pair(index, Anomaly(value, 1.0, detail)))
             }
         }
