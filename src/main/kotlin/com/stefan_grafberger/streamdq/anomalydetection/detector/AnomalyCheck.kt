a416505b178ac312d4205e0a1958a6c756fab703
Tong <tong.wu4@student.uva.nl>
2023-05-04 12:12:44 +0200

    Add onWindow and onAggregateMetric for AnomalyCheck

--- src/main/kotlin/com/stefan_grafberger/streamdq/anomalydetection/detector/AnomalyCheck.kt
+++ src/main/kotlin/com/stefan_grafberger/streamdq/anomalydetection/detector/AnomalyCheck.kt
@@ -1,22 +1,24 @@
 package com.stefan_grafberger.streamdq.anomalydetection.detector
 
+import com.stefan_grafberger.streamdq.anomalydetection.AnomalyDetectionStrategy
 import com.stefan_grafberger.streamdq.checks.Check
+import com.stefan_grafberger.streamdq.checks.TypeQueryableAggregateFunction
+import com.stefan_grafberger.streamdq.checks.aggregate.WindowAggregateCheck
+import org.apache.flink.api.common.ExecutionConfig
+import org.apache.flink.api.common.typeinfo.TypeInformation
 import org.apache.flink.streaming.api.windowing.assigners.WindowAssigner
 import org.apache.flink.streaming.api.windowing.windows.Window
 
-/**
- * TODO: windowing function to be designed and added,
- * TODO: need to ask stefan for some suggestions
- */
-class AnomalyCheck : Check() {
-    override fun equals(other: Any?): Boolean {
-        TODO("Not yet implemented")
+abstract class AnomalyCheck : Check() {
+
+    fun <W : Window> onWindow(aggregateWindowAssigner: WindowAssigner<Any?, W>)
+            : WindowAggregateCheck<W> {
+        return WindowAggregateCheck(aggregateWindowAssigner)
     }
 
-    override fun hashCode(): Int {
-        TODO("Not yet implemented")
-    }
-
-    fun <W : Window> onWindow(aggregateWindowAssigner: WindowAssigner<Any?, W>) {
+    fun <T> onAggregateMetric(anomalyDetectionStrategy: AnomalyDetectionStrategy,
+                              streamObjectTypeInfo: TypeInformation<T>, config: ExecutionConfig?)
+            : TypeQueryableAggregateFunction<T> {
+        return anomalyDetectionStrategy.getAggregateFunction(streamObjectTypeInfo, config)
     }
 }
