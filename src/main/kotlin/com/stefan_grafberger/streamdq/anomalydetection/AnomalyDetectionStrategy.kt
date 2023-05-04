96368e443e6285979f3ae1b3b1732fda424e6ada
Tong <tong.wu4@student.uva.nl>
2023-05-04 12:12:02 +0200

    Add getAggregateFunction and addWindowOrTrigger for AnomalyDetectionStrategy interface

--- src/main/kotlin/com/stefan_grafberger/streamdq/anomalydetection/AnomalyDetectionStrategy.kt
+++ src/main/kotlin/com/stefan_grafberger/streamdq/anomalydetection/AnomalyDetectionStrategy.kt
@@ -1,11 +1,34 @@
 package com.stefan_grafberger.streamdq.anomalydetection
 
 import com.stefan_grafberger.streamdq.anomalydetection.model.Anomaly
+import com.stefan_grafberger.streamdq.checks.TypeQueryableAggregateFunction
+import org.apache.flink.api.common.ExecutionConfig
+import org.apache.flink.api.common.typeinfo.TypeInformation
+import org.apache.flink.streaming.api.datastream.*
+import org.apache.flink.streaming.api.windowing.windows.Window
 
 /**
  * The common interface for all anomaly detection strategies
  */
 interface AnomalyDetectionStrategy {
-    fun detect(dataStream: List<Double>,
-               searchInterval: Pair<Int, Int>): MutableCollection<Pair<Int, Anomaly>>
+    fun detect(dataStream: List<Double>, searchInterval: Pair<Int, Int>)
+            : MutableCollection<Pair<Int, Anomaly>>
+
+    /**
+     * getAggregateFunction: return exact object, aggregation function
+     * from flink. to look: HllSketchCountDistinctAggregateTest
+     */
+    fun <R> getAggregateFunction(): SingleOutputStreamOperator<R>
+
+    fun <T> getAggregateFunction(streamObjectTypeInfo: TypeInformation<T>, config: ExecutionConfig?
+    ): TypeQueryableAggregateFunction<T>
+
+    /**
+     * getWindow: addWindowOrTriggerKeyed also non-keyed
+     */
+    fun <IN, KEY> addWindowOrTriggerKeyed(keyedStream: KeyedStream<IN, KEY>)
+            : WindowedStream<IN, KEY, Window>
+
+    fun <IN> addWindowOrTriggerNonKeyed(dataStream: DataStream<IN>, mergeKeyedResultsOnly: Boolean)
+            : AllWindowedStream<IN, Window>
 }
