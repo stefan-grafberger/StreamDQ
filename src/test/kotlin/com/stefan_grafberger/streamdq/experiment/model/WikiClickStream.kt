ef5ddceb4c3f5b068a4b2c5b4ef2617cf721954f
Tong <tong.wu4@student.uva.nl>
2023-06-24 12:02:43 +0200

    refactor: remove experiment from StreamDQ

--- src/test/kotlin/com/stefan_grafberger/streamdq/experiment/model/WikiClickStream.kt
+++
@@ -1,8 +0,0 @@
-package com.stefan_grafberger.streamdq.experiment.model
-
-data class WikiClickStream @JvmOverloads constructor(
-        var prev: String? = null,
-        var curr: String? = null,
-        var type: String? = null,
-        var count: String? = null,
-)
