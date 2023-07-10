bdf5bf856bcffad28b9ea83967f47c37e43c79e1
Tong <tong.wu4@student.uva.nl>
2023-07-10 12:56:14 +0200

    docs: Add KDoc for AnomalyDetector

--- src/main/kotlin/com/stefan_grafberger/streamdq/anomalydetection/detectors/AnomalyDetector.kt
+++ src/main/kotlin/com/stefan_grafberger/streamdq/anomalydetection/detectors/AnomalyDetector.kt
@@ -4,6 +4,17 @@ import com.stefan_grafberger.streamdq.anomalydetection.model.AnomalyCheckResult
 import org.apache.flink.streaming.api.datastream.DataStream
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator
 
+/**
+ * The interface for AnomalyDetector which is used
+ * for windowing the data stream source at the beginning
+ * and perform aggregate computation of data quality constraints
+ * if needed
+ *
+ * @Override by [com.stefan_grafberger.streamdq.anomalydetection.detectors.aggregatedetector]
+ *
+ * @author Tong Wu
+ * @since 1.0
+ */
 interface AnomalyDetector {
     fun <IN> detectAnomalyStream(dataStream: DataStream<IN>): SingleOutputStreamOperator<AnomalyCheckResult>
 }
